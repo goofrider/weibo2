f5cb465bff64c218cb3d5d1ae6c5479b2a093cf1
Jeffrey Chan <jeffrey.chan@hrichina.org>
2012-02-24 00:14:03 -0800

    Make Weibo2::Error gives meaningful messages when exception is raised.

--- lib/weibo2/error.rb
+++ lib/weibo2/error.rb
@@ -1,5 +1,6 @@
 module Weibo2
-  class Error < OAuth2::Error
+  class Error < StandardError
+    attr_reader :response, :code, :description
   
     def initialize(response)
       response.error = self
@@ -7,6 +8,7 @@ module Weibo2
       if response.parsed.is_a?(Hash)
         @code = response.parsed['error_code']
         @description = response.parsed['error']
+        super( "#{@description} (#{@code})" )
       end
     end
   end
